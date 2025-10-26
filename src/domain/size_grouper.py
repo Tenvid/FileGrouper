e8ca8c0599f2746520454e89313c79b13aeb0b9f
David <dagobar.pk.db@gmail.com>
2025-10-26 14:00:33 +0100

    ref: Improve SizeGrouper implementation
    - Add typehints
    - Make all numbers of slices float
    - Create prirvate methods for abstraction

--- src/domain/size_grouper.py
+++ src/domain/size_grouper.py
@@ -5,13 +5,13 @@ from pathlib import Path
 class SizeGrouper(Grouper):
         def group_files(self, origin_path: Path, destination_path: Path) -> None:
                 slices = [
-                        (0, 1),
-                        (1, 10),
-                        (10, 100),
-                        (100, 500),
-                        (500, 1024),
-                        (1024, 10 * 1024),
-                        (10 * 1024, float("inf")),
+                        (0.0, 1.0),
+                        (1.0, 10.0),
+                        (10.0, 100.0),
+                        (100.0, 500.0),
+                        (500.0, 1024.0),
+                        (1024.0, 10.0 * 1024.0),
+                        (10.0 * 1024.0, float("inf")),
                 ]  # TODO: Sizes should be entered by user
 
                 for slice in slices:
@@ -19,14 +19,29 @@ class SizeGrouper(Grouper):
                         slice_folder.mkdir(parents=True, exist_ok=True)
 
                 for file in origin_path.iterdir():
-                        if file.is_file():
-                                file_size_kb = file.stat().st_size / 1024
-                                for slice in slices:
-                                        if slice[0] < file_size_kb <= slice[1]:
-                                                target_folder = (
-                                                        destination_path
-                                                        / f"{slice[0]}-{slice[1]}KB"
-                                                )
-                                                target_path = target_folder / file.name
-                                                file.rename(target_path)
-                                                break
+                        if not file.is_file():
+                                continue
+                        file_size_kb = self.__get_file_size(file)
+                        for slice in slices:
+                                if self.__is_file_size_out_of_range(slice, file_size_kb):
+                                        continue
+                                target_path = self.__build_target_path(destination_path, slice, file)
+                                file.rename(target_path)
+                                break
+
+        def __is_file_size_out_of_range(self, slice: tuple[float, float], file_size_kb: float) -> bool:
+                return file_size_kb < slice[0] or file_size_kb > slice[1]
+
+        def __get_file_size(self, file: Path) -> float:
+                return file.stat().st_size / 1024
+
+        def __build_target_path(self, destination_path: Path, slice: tuple[float, float], file: Path) -> Path:
+                target_folder = self.__build_target_folder(
+                        destination_path,
+                        slice,
+                )
+                target_path = target_folder / file.name
+                return target_path
+
+        def __build_target_folder(self, destination_path: Path, slice: tuple[float, float]) -> Path:
+                return destination_path / f"{slice[0]}-{slice[1]}KB"

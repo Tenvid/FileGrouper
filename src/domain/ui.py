972de01461fba03f16e1016e4a92846bc4dfb4be
David <dagobar.pk.db@gmail.com>
2025-10-27 17:58:43 +0100

    feat: Add methods for handle ui attributes

--- src/domain/ui.py
+++ src/domain/ui.py
@@ -1,7 +1,16 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class UI(ABC):
         @abstractmethod
         def show(self) -> None:
                 """Render user interface."""
+
+        @abstractmethod
+        def store_value(self, variable_name: str, value: Any) -> None:
+                """Ask user to enter a value and store it in a custom variable name."""
+
+        @abstractmethod
+        def get_stored_values(self) -> dict[str, Any]:
+                """Return all stored values."""

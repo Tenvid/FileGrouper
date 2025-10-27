86f07bb6bcc4b576e8b0f86f725aee019fa2e918
David <dagobar.pk.db@gmail.com>
2025-10-27 17:48:53 +0100

    feat: Implement GroupFactory in main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 from src.application.GroupFilesUsecase import GroupFilesUsecase
-from src.domain.size_grouper import SizeGrouper
+from src.domain.group_factory import GroupFactory
+from src.domain.group_type import GroupType
 
 
 def main() -> None:
         raw_origin_path = input("Enter the origin folder path: ")
         raw_destination_path = input("Enter the destination folder path: ")
-        grouper = SizeGrouper()
+
+        grouper = GroupFactory().build(GroupType.SIZE)
         usecase = GroupFilesUsecase(
                 origin=Path(raw_origin_path),
                 destination=Path(raw_destination_path),

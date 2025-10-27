a26001b87a3e82ce7ae7d6d513acf50d9298dda6
David <dagobar.pk.db@gmail.com>
2025-10-27 18:20:50 +0100

    feat: Implement console ui in main

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ from pathlib import Path
 from src.application.GroupFilesUsecase import GroupFilesUsecase
 from src.domain.group_factory import GroupFactory
 from src.domain.group_type import GroupType
+from src.domain.console_ui import ConsoleUI
 
 
 def main() -> None:
         raw_origin_path = input("Enter the origin folder path: ")
         raw_destination_path = input("Enter the destination folder path: ")
 
-        grouper = GroupFactory().build(GroupType.SIZE)
+        ui = ConsoleUI()
+
+        ui.show()
+
+        ui_values = ui.get_stored_values()
+
+        grouper = GroupFactory().build(GroupType(ui_values["grouper_type"] - 1))
+
         usecase = GroupFilesUsecase(
                 origin=Path(raw_origin_path),
                 destination=Path(raw_destination_path),
                 grouper=grouper,  # TODO: Select Grouper implementation
         )
+
         usecase.execute()
 
 
